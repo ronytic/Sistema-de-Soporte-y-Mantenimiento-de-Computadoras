01f14d94eaff29e5bdc013b4c8cc05d82c920550
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-07-06 19:33:09 -0400

    Configuración Inicial

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$title="Sistema de Control de Soporte y Mantenimiento Tecnico del Organo Judicial";
+$siglas="";
+$directory="soportemantenimiento";//ej:carp/ sin el "/"
+$url="http://localhost/";//ej:http:"localhost/"
+$lema2="";
+$lema='Organo Judicial';
+$direccion="TRIBUNAL SUPREMO DE JUSTICIA";
+$telefono="2482516";
+
+$ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
+$tp="D3W1N8E0E4B4B4";
+?>
