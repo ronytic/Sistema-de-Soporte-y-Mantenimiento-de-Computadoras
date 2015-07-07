5fa8a44b20f08fda161a54e028e38fec66a294a5
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-07-06 20:08:22 -0400

    Menu

--- class/submenu.php
+++ class/submenu.php
@@ -6,9 +6,8 @@ class submenu extends bd{
 		$this->campos=array("*");
 		switch ($codUsuario) {
 			case 1:{return $this->getRecords("superadmin=1 and idmenu=$menu and activo=1","orden");}break;
-			case 2:{return $this->getRecords("gerente=1 and idmenu=$menu and activo=1","orden");}break;
-			case 3:{return $this->getRecords("encargadoventas=1 and idmenu=$menu and activo=1","orden");}break;
-			case 4:{return $this->getRecords("tecnicos=1 and idmenu=$menu and activo=1","orden");}break;
+			case 2:{return $this->getRecords("tecnico=1 and idmenu=$menu and activo=1","orden");}break;
+			case 3:{return $this->getRecords("usuario=1 and idmenu=$menu and activo=1","orden");}break;
 		}
 	}
 }
